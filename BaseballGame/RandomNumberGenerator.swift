b35e25563fff8c144317a8e6fe69854d4ca20558
NH <mnh4140@gmail.com>
2025-03-20 15:19:57 +0900

    refactor: 중복되지 않는 랜덤 세자리 수 생성 메소드 변경
    - 기존 repeat-while 문 대신 Set을 사용하여 구현

--- BaseballGame/RandomNumberGenerator.swift
+++ BaseballGame/RandomNumberGenerator.swift
@@ -15,6 +15,15 @@ struct RandomNumberGenerator: RandomNumberGeneratable {
      - Returns: 중복되지 않는 랜덤으로 생성한 3개의 숫자를 배열로 반환
     */
     func randomNumberGenerator() -> [Int] {
+        // Set을 활용하여 중복 숫자 제거
+        var numbers = Set<Int>()
+        
+        numbers.insert(Int.random(in: 1...9)) // 첫번째 수는 1부터 9까지
+        while numbers.count < 3 {
+            numbers.insert(Int.random(in: 0...9)) // 두번째 수부터 0부터 9까지
+        }
+        
+        /* repeat while 문을 이용한 중복되지 않는 세자리수 생성
         let randomNumber1 = Int.random(in: 1...9)
         var randomNumber2: Int
         var randomNumber3: Int
@@ -28,11 +37,12 @@ struct RandomNumberGenerator: RandomNumberGeneratable {
         repeat {
             randomNumber3 = Int.random(in: 0...9)
         } while randomNumber3 == randomNumber1 || randomNumber3 == randomNumber2
+        */
         
         // 디버깅
         //let test = [randomNumber1, randomNumber2, randomNumber3]
         //print(test)
         
-        return [randomNumber1, randomNumber2, randomNumber3]
+        return Array(numbers)
     }
 }
